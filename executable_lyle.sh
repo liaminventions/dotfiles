cba075ee6bdd8dad6842ea80e19bb050706aea8e
Waverider <unstinkable919@gmail.com>
2023-10-17 12:02:22 -0400

    lol

--- executable_lyle.sh
+++ executable_lyle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #hyprctl keyword monitor ",preferred,auto,4"
 #hyprctl keyword monitor ",480x270@60.00,auto,1"    
-wine ~/Downloads/lyle2/Lyle\ in\ Cube\ Sector/Lyle\ in\ Cube\ Sector.exe
+wine ~/games/Lyle\ in\ Cube\ Sector/Lyle\ in\ Cube\ Sector.exe
 #sleep 1
 #hyprctl keyword monitor ",preferred,auto,1"
