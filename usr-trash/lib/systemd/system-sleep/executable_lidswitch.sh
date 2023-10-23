be71845a6a8e63c6a42f4ede2dc844aa9cc390ca
Waverider <unstinkable919@gmail.com>
2023-10-22 22:11:28 -0400

    better hibernate

---
+++ usr-trash/lib/systemd/system-sleep/executable_lidswitch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+
+case "$1" in
+    pre)
+      /usr/bin/rm /home/waverider/.cache/power/suspend.lock
+    ;;
+    post)
+      /usr/bin/touch /home/waverider/.cache/power/suspend.lock
+    ;;
+esac
+
+exit 0
