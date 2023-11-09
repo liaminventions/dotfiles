23e9599b005fe16f3e0b650528789e3f9e77b21e
Waverider <unstinkable919@gmail.com>
2023-11-08 22:28:28 -0500

    final version of voice dictation script

--- executable_voice.sh
+++ executable_voice.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 CACHE_DIR="$HOME/.cache/voice"
+mkdir $CACHE_DIR
+function begin() {
+  rm "$CACHE_DIR/voice.lock"
+  nerd-dictation begin --simulate-input-tool WTYPE 
+}
+function end() {
+  touch "$CACHE_DIR/voice.lock"
+  nerd-dictation end
+}
+[ -f "$CACHE_DIR/voice.lock" ] && begin || end
 
-mkdir CACHE_DIR
-[ -f "$CACHE_DIR/voice.lock" ] && nerd-dictation begin --simulate-input-tool WTYPE || nerd-dictation end
