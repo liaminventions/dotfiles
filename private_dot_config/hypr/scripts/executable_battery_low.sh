e8f2095b7f5c964f10655070bf5cd70f226e3eec
Waverider <unstinkable919@gmail.com>
2024-06-21 15:49:09 -0400

    the

--- private_dot_config/hypr/scripts/executable_battery_low.sh
+++ private_dot_config/hypr/scripts/executable_battery_low.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 while true; do
-  battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-  if [ $battery_level -le 10 ]; then
-    notify-send "battery low" "(${battery_level}%)"
-  fi
-  if [ $battery_level -le 5 ]; then
-    notify-send "batty very low" "(${battery_level}%!)"
+  acpi -b | grep "Charging"
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+    if [ $battery_level -le 10 ]; then
+      notify-send "battery low" "(${battery_level}%)"
+    elif [ $battery_level -le 5 ]; then
+      notify-send "batty very low" "(${battery_level}%!)"
+    fi
   fi
   sleep 60
 done
