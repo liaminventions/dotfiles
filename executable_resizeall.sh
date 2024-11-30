991c6b860afae8a7d833ea67df21faeac3f02c99
Waverider <unstinkable919@gmail.com>
2024-11-30 18:10:24 -0500

    oops old ver

--- executable_resizeall.sh
+++ executable_resizeall.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-res=$1
-shift
-for file in "$@"
-do
-  ~/resize.sh "$file" $res
-done
+#!/bin/bash
 
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <resolution> <file1> <file2> ..."
+  exit 1
+fi
+
+res="$1"; shift
+
+for file in "$@"; do
+  if [ -f "$file" ]; then
+    ~/resize.sh "$file" "$res"
+  else
+    echo "'$file' does not exist; skipping..."
+  fi
+done
