099a8167c9c687e979dab73f4fb1ce1e5b0e70cb
Waverider <unstinkable919@gmail.com>
2024-12-05 19:36:53 -0500

    subjects: add cachesetup

---
+++ subjects/executable_cachesetup_remove.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+
+CS="$HOME/.config/hypr/scripts/cachesetup"
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <subject>"
+  exit 1
+fi
+
+SUBJECT="$1"
+
+cp "$CS" "${CS}.bak"
+
+sed -i "/touch ~\/.cache\/eww\/${SUBJECT}.lock/d" "$CS"
+
+echo "Removed $SUBJECT from $CS"
