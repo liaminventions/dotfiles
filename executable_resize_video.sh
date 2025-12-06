13a64816fe1a58f3e968d76c3bc5c22654ce2c91
waverider <unstinkable919@gmail.com>
2025-12-05 22:28:48 -0500

    upd resize to av1 2

--- executable_resize_video.sh
+++ executable_resize_video.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # usage: ./resize_video.sh input.mp4 output.mp4 target_size_mb
 
@@ -10,4 +11,4 @@ duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wr
 
 bitrate=$(echo "(($mb*1024*1024*8)/$duration)/1000"|bc)"k"
 
-ffmpeg -i $in -b:v $bitrate -fflags +genpts -r 60 -y $out
+ffmpeg -i "$in" -c:v libsvtav1 -b:v $bitrate -fflags +genpts -r 60 -y "$out"
